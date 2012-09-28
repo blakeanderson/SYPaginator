84e9a8a85c815036b6d251f697e19243e241ee67
Blake Anderson <blakeanderson.cs@gmail.com>
2012-09-28 11:27:52 -0600

    Only show page text label in Debug

--- SYPaginator/SYPageControl.m
+++ SYPaginator/SYPageControl.m
@@ -25,11 +25,12 @@ static NSInteger const kSYPageControlMaxNumberOfDots = 0;
 @synthesize pageControl = _pageControl;
 @synthesize textLabel = _textLabel;
 
+
 - (void)setNumberOfPages:(NSInteger)numberOfPages {
 	_numberOfPages = numberOfPages;
 	if (numberOfPages == 1 && _hidesForSinglePage) {
 		[_pageControl removeFromSuperview];
-		[_textLabel removeFromSuperview];
+    [self removeTextLabelFromView];
 		return;
 	}
 	
@@ -38,17 +39,20 @@ static NSInteger const kSYPageControlMaxNumberOfDots = 0;
 		if (!_pageControl.superview) {
 			[self addSubview:_pageControl];
 		}
-		[_textLabel removeFromSuperview];
+    
+    [self removeTextLabelFromView];
 	} else {
 		_pageControl.numberOfPages = 0;
 		[_pageControl removeFromSuperview];
 		
+    #ifdef DEBUG
 		[self _updateTextLabel];		
 		if (!_textLabel.superview) {
 			[self addSubview:_textLabel];
 		}
+    #endif
 	}
-	
+
 	[self setNeedsLayout];
 }
 
@@ -64,29 +68,23 @@ static NSInteger const kSYPageControlMaxNumberOfDots = 0;
 	if (_numberOfPages <= kSYPageControlMaxNumberOfDots) {
 		_pageControl.currentPage = currentPage;
 	} else {
-		[self _updateTextLabel];
+    
+  #ifdef DEBUG
+      [self _updateTextLabel];    
+  #endif
+		
+
 	}
 }
 
 
-#pragma mark - UIView
+#pragma mark - UIView+   
 
 - (id)initWithFrame:(CGRect)frame {
 	if ((self = [super initWithFrame:frame])) {
 		_pageControl = [[UIPageControl alloc] initWithFrame:CGRectZero];
 		[_pageControl addTarget:self action:@selector(_pageControlChanged:) forControlEvents:UIControlEventValueChanged];
 		
-		_textLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-		_textLabel.font = [UIFont boldSystemFontOfSize:12.0f];
-		_textLabel.textColor = [UIColor whiteColor];
-		_textLabel.backgroundColor = [UIColor clearColor];
-		_textLabel.textAlignment = UITextAlignmentCenter;
-		_textLabel.shadowColor = [UIColor colorWithWhite:0.0f alpha:0.5f];
-		_textLabel.shadowOffset = CGSizeMake(0.0f, 1.0f);
-		_textLabel.userInteractionEnabled = YES;
-		
-		UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_labelTapped:)];
-		[_textLabel addGestureRecognizer:tap];
 	}
 	return self;
 }
@@ -94,7 +92,9 @@ static NSInteger const kSYPageControlMaxNumberOfDots = 0;
 
 - (void)layoutSubviews {
 	_pageControl.frame = self.bounds;
-	_textLabel.frame = self.bounds;
+  #ifdef DEBUG
+    self.textLabel.frame = self.bounds;
+  #endif
 }
 
 
@@ -113,11 +113,11 @@ static NSInteger const kSYPageControlMaxNumberOfDots = 0;
 	NSString *format = SYPaginatorLocalizedString(@"PAGE_OF_TOTAL");
 	if (!format) {
 		NSLog(@"[SYPaginator] WARNING: SYPaginatorResources.bundle is missing. Please add it your copy resources build phase.");
-		_textLabel.text = nil;
+		self.textLabel.text = nil;
 		return;
 	}
 	
-	_textLabel.text = [NSString stringWithFormat:format,
+	self.textLabel.text = [NSString stringWithFormat:format,
 					   [numberFormatter stringFromNumber:[NSNumber numberWithInteger:_currentPage + 1]],
 					   [numberFormatter stringFromNumber:[NSNumber numberWithInteger:_numberOfPages]]];
 }
@@ -133,6 +133,31 @@ static NSInteger const kSYPageControlMaxNumberOfDots = 0;
 	BOOL increment = [gestureRecognizer locationInView:self].x >= self.bounds.size.width / 2.0f;
 	self.currentPage += increment ? 1 : -1;
 	[self sendActionsForControlEvents:UIControlEventValueChanged];
+}
+
+#pragma mark - Bottom Text Label
+
+- (UILabel*)textLabel{
+  if (!_textLabel){
+    _textLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+		_textLabel.font = [UIFont boldSystemFontOfSize:12.0f];
+		_textLabel.textColor = [UIColor whiteColor];
+		_textLabel.backgroundColor = [UIColor clearColor];
+		_textLabel.textAlignment = UITextAlignmentCenter;
+		_textLabel.shadowColor = [UIColor colorWithWhite:0.0f alpha:0.5f];
+		_textLabel.shadowOffset = CGSizeMake(0.0f, 1.0f);
+		_textLabel.userInteractionEnabled = YES;
+		
+		UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_labelTapped:)];
+		[_textLabel addGestureRecognizer:tap];
+  }
+  return _textLabel;
+}
+
+- (void)removeTextLabelFromView{
+#ifdef DEBUG
+  [self.textLabel removeFromSuperview];
+#endif
 }
 
 @end
